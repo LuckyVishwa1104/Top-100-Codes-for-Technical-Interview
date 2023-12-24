d4dcad48cfe30be737f57afdb3dccd71cc51ae39
Lucky Vishwakarma <110558983+LuckyVishwa1104@users.noreply.github.com>
2023-12-24 10:58:23 +0530

    added two more methods to solve octal to binary conversion

--- 38_octal_to_binary.py
+++ 38_octal_to_binary.py
@@ -1,27 +1,56 @@
 # Python program to convert octal number into its equvalent binary number
 
-# method 1 - brut-force
-import sys
-n=int(input("Enter the octal number :"))
-bina=""
-def octal_to_binary(n):
-    if n<=0:
-        return print(end="")
+# method 1 - using brut-force
+n=int(input("Enter the octal unmber :"))
+octl=""
+while n>0:
     a=n%10
+    j=0
     srt=""
-    def binary(a,j):
-        if j>=3:
-            return print(end="")
-        m=a%2
-        nonlocal srt
-        srt=str(m)+srt
-        binary(a//2,j+1)
-        pass
-    binary(a,0)
-    global bina
-    bina=srt+bina
-    octal_to_binary(n//10)
-    pass
-octal_to_binary(n)
-print(int(bina))
+    while j<3:
+        b=a%2
+        srt=str(b)+srt
+        j=j+1
+        a=a//2
+    octl=srt+octl
+    n=n//10
+print(int(octl))
 
+# method 2 - using recursion
+# import sys
+# n=int(input("Enter the octal number :"))
+# bina=""
+# def octal_to_binary(n):
+#     if n<=0:
+#         return print(end="")
+#     a=n%10
+#     srt=""
+#     def binary(a,j):
+#         if j>=3:
+#             return print(end="")
+#         m=a%2
+#         nonlocal srt
+#         srt=str(m)+srt
+#         binary(a//2,j+1)
+#         pass
+#     binary(a,0)
+#     global bina
+#     bina=srt+bina
+#     octal_to_binary(n//10)
+#     pass
+# octal_to_binary(n)
+# print(int(bina))
+
+# method 3 - using built-in method
+n=int(input("Enter the octal number :"))
+bina=bin(int(n,8))
+print(bina)
+# srt=''
+# for i in range(1,11):
+#     srt=srt+str(i)
+# print(srt)
+
+# srtt=''
+# for i in range(1,11):
+#     srtt=str(i)+srtt
+# print(srtt)
