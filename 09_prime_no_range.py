8dd4c47f1d3e65e6bc20a9246a937f002b4385cc
Lucky Vishwakarma <110558983+LuckyVishwa1104@users.noreply.github.com>
2024-01-02 12:20:53 +0530

    added one more mrthod

--- 09_prime_no_range.py
+++ 09_prime_no_range.py
@@ -23,3 +23,18 @@ try:
             print(i)
 except:
     print("Enter valid integer value")
+
+# method 3- compairing the number of divisor
+# prime number usually have number of divisor equal to two
+try:
+    n=int(input("Enter the number :"))
+    m=2
+    for i in range(1,n+1):
+        cnt=0
+        for j in range(1,i+1):
+            if i%j==0:
+                cnt=cnt+1
+        if cnt==2:
+            print(i)
+except:
+    print("Something went wrong!!!")
