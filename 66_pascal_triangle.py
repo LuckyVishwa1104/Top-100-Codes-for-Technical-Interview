f5a20820e6dbce88911c792d0bdbb19736e6990e
Lucky Vishwakarma <110558983+LuckyVishwa1104@users.noreply.github.com>
2024-08-03 21:55:44 +0530

    added the OOP based approach to solve given problem

--- 66_pascal_triangle.py
+++ 66_pascal_triangle.py
@@ -14,7 +14,7 @@ for i in range(n+1):
         g = combinaiton(i,j)
         l2.append(g)
     l1.append(l2)
-print(l1)
+print("Iteration approach :",l1)
 
 # method 2 - using recursion
 m = int(input("Enter the number :"))
@@ -34,4 +34,58 @@ def func1(p1):
     func1(p1+1)
     pass
 func1(0)
-print(l3)
+print("Recursion appraoch :",l3)
+
+# method 3 - using object oriented approach
+try:
+    class PascalTriangle:
+        def __init__(self,num):
+            self.num = num
+            self.l1 = []
+    
+        def func1(self,n1):
+            fac1 = 1
+            def factorial(b):
+                nonlocal fac1
+                if b ==0 :
+                    return
+                fac1 = fac1 * b
+                factorial(b-1)
+                pass
+
+            def fac(a): 
+                factorial(a)  
+                return fac1
+            
+            return fac(n1)
+    
+        def combination(self,a1,b1):
+            c = self.func1(a1)//(self.func1(b1)*self.func1(a1-b1))
+            return c
+    
+        def pascalTriangle(self):
+            def func1(p1):
+                if p1 >= self.num+1:
+                    return
+                l4 = []
+                def func2(q1):
+                    if q1 >= p1+1:
+                        return
+                    r1 = self.combination(p1,q1)
+                    l4.append(r1)
+                    func2(q1+1)
+                func2(0)
+                self.l1.append(l4)
+                func1(p1+1)
+            func1(0)
+
+    number = int(input("Enter the number :"))
+    obj1 = PascalTriangle(number)
+    obj1.pascalTriangle()
+    import sys
+    sys.stdout.write("OOP approach :")
+    sys.stdout.write(str(obj1.l1))
+    sys.stdout.write('\n')
+    pass
+except:
+    print("Enter a valid input value!")
