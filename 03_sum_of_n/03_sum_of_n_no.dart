e527ba7b48f7eb63226d7f3bb406f1c53a3d674f
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-05-06 23:37:52 +0530

    feat(for-loop): add for loop for sum calculation

--- 03_sum_of_n/03_sum_of_n_no.dart
+++ 03_sum_of_n/03_sum_of_n_no.dart
@@ -4,6 +4,15 @@ int sumOfNNumbers(int num) {
   return (num * (num + 1) ~/ 2);
 }
 
+int sumOfNNumbersLoop(int num){
+  int sum = 0;
+  for (int i = 0; i <= num; i++){
+    sum= sum + i;
+  }
+
+  return sum;
+}
+
 void main() {
   try {
     stdout.write('Enter a valid Integer value : ');
@@ -12,7 +21,7 @@ void main() {
     if (num < 0) {
       print('Enter a valid positive Integer value.');
     } else {
-      int result = sumOfNNumbers(num);
+      int result = sumOfNNumbersLoop(num);
       print("Sum of First $num numbers : $result");
     }
   } on FormatException catch (e) {
