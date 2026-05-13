7494227361aede24b065ee1f5adaf3533aa019b3
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-05-13 22:22:31 +0530

    feat(logic): add modular approach

--- 09_prime_no_range/09_prime_no_range.py
+++ 09_prime_no_range/09_prime_no_range.py
@@ -38,3 +38,41 @@ try:
             print(i)
 except:
     print("Something went wrong!!!")
+
+
+# method 4 - function based approach
+
+def prime_range(num):
+    cnt = num + 1
+    prime_list = []
+    for i in range(2, cnt): 
+        temp = False
+        cnt2 = int(i ** 0.5) + 1
+        for j in range(2, cnt2):
+            if (i % j == 0):
+                temp = True
+                break
+        if(temp == False):
+            prime_list.append(i)
+    return prime_list
+
+try:
+    while (True):
+        num = int(input("Enter a integer value : "))
+        if (num <= 1):
+            print("Enter a positive integer value number greate than two")
+        else:
+            result = prime_range(num)
+            print(result)
+
+        choice = input("Do you want to continue the program? (y/n) : ")
+        if (choice.lower() == 'n'):
+            print("Program finished!")
+            break
+
+except ValueError as e:
+    print(f"Invalid Input : {e}")
+
+except Exception as e:
+    print(f"Exception caught : {e}")
+
