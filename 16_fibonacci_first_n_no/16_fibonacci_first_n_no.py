0a4bfba7a197133c6a54ffee11904a3ae11d0114
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-05-20 11:51:05 +0530

    feat(logic): add functin based implementation

--- 16_fibonacci_first_n_no/16_fibonacci_first_n_no.py
+++ 16_fibonacci_first_n_no/16_fibonacci_first_n_no.py
@@ -34,3 +34,35 @@ try:
     print(fibb(n1,n2))
 except:
     print('Enter a valid integer value.')
+
+# method 3 - using function based approach
+
+def fibonacci_series(num):
+    n1 = 0
+    n2 = 1
+    fibo_series = [n1]
+    
+    for i in range(num+1):
+        fibo_series.append(n2)
+        n3 = n1 + n2
+        n1 = n2
+        n2 = n3
+
+    return fibo_series
+
+try:
+    while (True):
+        num = int(input("Enter positive integer number : "))
+
+        if(num < 0):
+            print("Enter positive integer value")
+
+        else:
+            result = fibonacci_series(num)
+            print(result)
+
+except ValueError as e:
+    print("Invalid Input : {e}")
+
+except Exception as e:
+    print("Exception caught : {e}")
