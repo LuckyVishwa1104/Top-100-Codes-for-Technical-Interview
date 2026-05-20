b81f01d06834f3dfeabb05dbb58521a175af477c
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-05-20 11:05:02 +0530

    feat(logic): add mobule based approach

--- 15_fibonacci_series/15_fibonacci_series.py
+++ 15_fibonacci_series/15_fibonacci_series.py
@@ -32,5 +32,37 @@ try:
     print(fibb(n1,n2))
 except:
     print("Enter a valid integer value number.")
-    
+
+# method 3 - function based approach
+def fibonacci_series(num):
+    n1 = 0
+    fibo_series = [n1]
+    n2 = 1
+    while (n2 <= num):
+        fibo_series.append(n2)
+        n3 = n1 + n2
+        n1 = n2
+        n2 = n3
+    return fibo_series
+
+try:
+    while(True):
+        num = int(input("Enter range value : "))
+        if (num <= 1):
+            print("Enter integer value greater than 1")
+        else:
+            result = fibonacci_series(num)
+            print(result)
+
+        choice = input("Do you want to continue the program? (y/n) : ")
+        if (choice.lower() == 'n'):
+            print("Program finished!")
+            break
+
+except ValueError as e:
+    print(f"Invalid Input : {e}")
+
+except Exception as e:
+    print(f"Exception caught : {e}")
+
 
