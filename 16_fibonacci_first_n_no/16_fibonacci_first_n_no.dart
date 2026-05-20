716d68aa4ab0ee4878dc5fa6a8c60c58c3dcf4d8
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-05-20 11:51:30 +0530

    feat(logic): add dart implementaion

---
+++ 16_fibonacci_first_n_no/16_fibonacci_first_n_no.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+// program to find first n numbers in fibonacci series
+
+List<int> fibonacciSeries(int num){
+  int n1 = 0;
+  int n2 = 1;
+  List<int> fiboSeries = [n1];
+
+  for (int i = 0; i <= num; i++){
+    fiboSeries.add(n2);
+    int n3 = n1 + n2;
+    n1 = n2;
+    n2 = n3;
+  }
+
+  return fiboSeries;
+}
+
+void main(){
+
+  try{
+
+    while(true){
+      stdout.write("Enter range value : ");
+      int num = int.parse(stdin.readLineSync()!);
+
+      if (num < 0) {
+        print("Enter integer value greater than 0");
+      } else {
+        List<int> result = fibonacciSeries(num);
+        print(result);
+      }
+    }
+
+  } on FormatException catch (e) {
+    print("Invalid input : $e");
+  } on Exception catch (e) {
+    print("Exception caught : $e");
+  }
+
+}
