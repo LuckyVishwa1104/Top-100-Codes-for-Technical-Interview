e9b6ead8ad26f635f1165bc29723371796a14993
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-05-29 19:27:41 +0530

    feat(logic): add module based approach

--- 21_strong_number/21_strong_number.py
+++ 21_strong_number/21_strong_number.py
@@ -42,3 +42,48 @@ try:
         print(f"{n2} is not a Strong number")
 except:
     print("Enter a valid integer value")
+
+# method 3 - using module based approach
+
+def fac(num):
+    fact = 1
+    for i in range(1, num+1):
+        fact = fact * i
+    return fact
+
+def is_strong(num):
+    strong_value = 0
+    temp = num
+    while num > 0:
+        strong_value = strong_value + fac(num % 10)
+        num = num // 10
+    if (temp == strong_value):
+        return True
+    return False
+
+try:
+    while(True):
+        num = int(input("Enter a integer value : "))
+        if (num <= 0):
+            print("Enter positiveinteger value")
+        else:
+            result = is_strong(num)
+            if (result):
+                print(f"{num} is a strong number")
+            else:
+                print(f"{num} is not a strong number")
+
+        choice = input("Do you want to continue the program (y/n) : ")
+        if (choice.lower() == "n"):
+            print("Program Finiched!")
+            break
+
+except ValueError as e:
+    print(f"Invalid input : {e}")
+
+except ZeroDivisionError as e:
+    print(f"Zero division exception : {e}")
+
+except Exception as e:
+    print(f"Exception caught : {e}")
+
