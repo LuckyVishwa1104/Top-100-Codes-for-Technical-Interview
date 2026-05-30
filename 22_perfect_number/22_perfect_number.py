aed4b6f28e31f0d35dffaf7d5a1ff6593fc93b13
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-05-30 22:27:02 +0530

    feat(logic): add module based approach for the problem statement

--- 22_perfect_number/22_perfect_number.py
+++ 22_perfect_number/22_perfect_number.py
@@ -33,3 +33,44 @@ try:
         print(f'{m} is not a perfect number.')
 except:
     print("Enter a valid integer value")
+
+# method 3 - using module based approach
+
+def isPerfect(num):
+    summ = 0
+    for i in range(1, num):
+        if (num % i == 0):
+            summ = summ + i
+    if (summ == num):
+        return True
+    return False
+
+try:
+    while(True):
+        num = int(input("Enter positive integer value : "))
+        limit  = 1000
+        if (num <= 0):
+            print("Enter positiveinteger value")
+        elif (num > limit):
+            print("Input too large")
+        else:
+            result = isPerfect(num)
+            if(result):
+                print(f"{num} is a Perfect number")
+            else:
+                print(f"{num} is not a perfect number")
+        
+        choice = input("Do you want to continue the program (y/n) : ")
+        if (choice.lower() == "n"):
+            print("Program Finiched!")
+            break
+
+except ValueError as e:
+    print(f"Invalid input : {e}")
+
+except ZeroDivisionError as e:
+    print(f"Zero division exception : {e}")
+
+except Exception as e:
+    print(f"Exception caught : {e}")
+
