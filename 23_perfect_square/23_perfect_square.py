b5bfa0e7d6efb1c3a31856c2b0663b6178d78b6c
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-05-31 19:47:30 +0530

    feat(logic): add module based approach for perfect square number

--- 23_perfect_square/23_perfect_square.py
+++ 23_perfect_square/23_perfect_square.py
@@ -18,4 +18,42 @@ if m == l:
 else:
     print(f"{n} not a Perfect Square")
 
+# method 3 - module based approach
 
+def is_perfect_square(num):
+    square_root = int(num ** (0.5))
+    if (square_root * square_root):
+        return True
+    return False
+
+try:
+    while(True):
+
+        num = int(input("Enter positive integer value : "))
+        limit  = 1000
+        if (num <= 0):
+            print("Enter positiveinteger value")
+        elif (num > limit):
+            print("Input too large")
+        else:
+            result = is_perfect_square(num)
+            if(result):
+                print(f"{num} is a perfect square")
+            else:
+                print(f"{num} is not a perfect square")
+
+        choice = input("Do you want to continue the program (y/n) : ")
+        if(choice == "n"):
+            print("Program finished!")
+            break
+
+except ValueError as e:
+    print("Invalid Input : {e}")
+
+except ZeroDivisionError as e:
+    print(f"Zero division exception : {e}")
+
+except Exception as e:
+    print("Exception caught : {e}")
+
+    
