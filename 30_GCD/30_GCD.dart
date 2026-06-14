8ca25b32bc5695c9519795eab4c4d37c48506d32
Nikita <236742122+nv-2000@users.noreply.github.com>
2026-06-14 21:09:11 +0530

    feat(logic): add dart implementaion

---
+++ 30_GCD/30_GCD.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+import 'dart:math';
+
+// GDC - greates common divisor - dart program to finf the gdc of two numbers
+
+int gcd(int num1, int num2) {
+  int minn = min(num1, num2);
+  for (int i = 0; i <= minn; i++) {
+    if (num1 % i == 0 && num2 % i == 0) {
+      break;
+    }
+  }
+  return minn;
+}
+
+void main() {
+  try {
+    while (true) {
+      stdout.write("Enter a positive integer value : ");
+      int num1 = int.parse(stdin.readLineSync()!);
+
+      stdout.write("Enter a positive integer value : ");
+      int num2 = int.parse(stdin.readLineSync()!);
+
+      int maxLimit = 1000;
+
+      if (num1 > maxLimit || num2 > maxLimit) {
+        print("Enter a lesser value than 10000");
+      } else if (num1 == 0 || num2 == 0) {
+        print("Enter a valid integer value : ");
+      } else {
+        int result = gcd(num1, num2);
+        print("GCD = $result");
+      }
+
+      stdout.write("Do you want to continue the program (y/n) : ");
+      String choice = stdin.readLineSync()!.toLowerCase();
+      if (choice == 'n') {
+        print("Program finished!");
+        break;
+      }
+    }
+  } on FormatException catch (e) {
+    print("Invalid input : $e");
+  } on UnsupportedError catch (e) {
+    print("Divide by zero exception : $e");
+  } on Exception catch (e) {
+    print("Exception caught : $e");
+  }
+}
