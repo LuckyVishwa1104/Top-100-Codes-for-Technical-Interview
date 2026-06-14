159dd01c02061ae301ba449c6726845ec9e03580
Nikita <236742122+nv-2000@users.noreply.github.com>
2026-06-14 21:08:22 +0530

    feat(logic): add module based appraoch for GCD logic

--- 30_GCD/30_GCD.py
+++ 30_GCD/30_GCD.py
@@ -19,3 +19,38 @@ def fun1(i):
     fun1(i+1)
 fun1(1)
 print(gcd,"is the GCD")
+
+# method 3 - using module based approach
+
+def gdc(num1, num2):
+    minn = min([num1,num2])
+    for i in range(1, minn + 1):
+        if (num1%minn and num2%minn):
+            return minn
+
+try:
+    while(True):
+        num1 = int(input("Enter positive integer value : "))
+        num2 = int(input("Enter positive integer value : "))
+        limit  = 1000
+        if (num1 == 0 or num2 == 0):
+            print("Enter positive integer value")
+        elif (num1 > limit or num2 > limit):
+            print("Input too large")
+        else:
+            result = gdc(num1, num2)
+            print(f"GCD = {result}")
+    
+        choice = input("Do you want to continue the program (y/n) : ")
+        if(choice == "n"):
+            print("Program finished!")
+            break
+
+except ValueError as e:
+    print("Invalid Input : {e}")
+
+except ZeroDivisionError as e:
+    print(f"Zero division exception : {e}")
+
+except Exception as e:
+    print(f"Exception caught : {e}")
