79fe900297b286356d94de5cd6050c65c7d1346f
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-07-02 10:09:26 +0530

    feat(logic): add module based approach for quadrant checking

--- 39_quadrant_checking/39_quadrant_checking.py
+++ 39_quadrant_checking/39_quadrant_checking.py
@@ -28,3 +28,52 @@ if __name__=="__main__":
     x=int(input("Enter the first co-ordinate :"))
     y=int(input("Enter the second co-ordinate :"))
     fun(x,y)
+
+# method 2 - using module based appraoch
+
+def check_quadran(x, y):
+    if (x == 0 and y == 0):
+        return "Origin"
+    
+    if (x == 0):
+        return "Posivite Y-axis" if y > 0 else "Negative Y-axis"
+    
+    if (y == 0):
+        return "Positive X-axis" if x > 0 else "Negative X-axis"
+    
+    if (x > 0 and y > 0):
+        return "First Quadrant"
+    
+    if (x < 0 and y < 0):
+        return "Third Quadrant"
+    
+    if (x > 0 and y < 0):
+        return "Fourth Quadrant"
+    
+    if (x < 0 and y > 0):
+        return "Second Quadrant"
+    
+    return "Lies somewhere universe"
+
+# driver program 
+try:
+    while (True):
+        num1 = int(input("Enter integer value : "))
+        num2 = int(input("Enter integer value : "))
+
+        result = check_quadran(num1, num2)
+        print(f"({num1}, {num2}) lies in {result}")
+
+        choice = input("Do you want to continue the program (y/n) : ")
+        if(choice.lower() == "n"):
+            print("Program finished!")
+            break
+
+except ValueError as e:
+    print(f"Invalid Input : {e}")
+
+except ZeroDivisionError as e:
+    print(f"Zero division exception : {e}")
+
+except Exception as e:
+    print(f"Exception caught : {e}")
