41843e5d8f07dae0eb36c1ae361dd799ff935f59
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-07-04 10:13:55 +0530

    feat(logic): add module based approach for max handshake problem

--- 41_maximum_handshake/41_maximum_handshake.py
+++ 41_maximum_handshake/41_maximum_handshake.py
@@ -14,3 +14,35 @@ sum=0
 for i in range(1,n):
     sum=sum+i
 print("Maximum shakehand will be",sum)
+
+# method 3 - using module based approach
+
+# lambda function for calculating max handshake
+max_handshake = lambda num : (num*(num-1))/2
+
+# driver program 
+try:
+    while (True):
+        num1 = int(input("Enter number of Persons : "))
+
+        if (num1 <= 0):
+            print("Enter positive integer values")
+        
+        else:
+            result = max_handshake(num1)
+            print(f"{num1} hand shake are possible!")
+
+        choice = input("Do you want to continue the program (y/n) : ")
+        if(choice.lower() == "n"):
+            print("Program finished!")
+            break
+
+except ValueError as e:
+    print(f"Invalid Input : {e}")
+
+except ZeroDivisionError as e:
+    print(f"Zero division exception : {e}")
+
+except Exception as e:
+    print(f"Exception caught : {e}")
+
