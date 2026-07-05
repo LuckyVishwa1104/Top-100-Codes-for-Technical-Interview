5cf1f7ce0d5ba6efb6b34298901b23dc5a99433c
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-07-06 01:30:33 +0530

    feat(logic): add module based approach for sum of fraction

--- 42_sum_fraction/42_sum_fraction.py
+++ 42_sum_fraction/42_sum_fraction.py
@@ -16,3 +16,48 @@ elif a==0 and c==0:
 else:
     print(f"{(a*d)+(b*c)}/{b*d}")
 
+# method 2 - using module based approach
+
+def euclidean_hfc(num1, num2):
+    while num2 > 0:
+        temp = num1
+        num1 = num2
+        num2 = temp % num2
+    return num1
+
+def fractional_sum(nume1, deno1, nume2, deno2):
+    nume3 = (nume1 * deno2) + (deno1 * nume2)
+    deno3 = deno1 * deno2
+    hcf = euclidean_hfc(abs(nume3), abs(deno3))
+    nume3 = nume3 // hcf
+    deno3 = deno3 // hcf
+    if deno3 < 0:
+        deno3 *= -1
+        nume3 *= -1
+    return nume3, deno3
+
+try:
+    while (True):
+        nume1, deno1 = map(int, input("Enter 1st fraction : ").split("/"))
+        nume2, deno2 = map(int, input("Enter 2nd fraction : ").split("/"))
+
+        if (deno1 == 0 or deno2 == 0):
+            print("Denominator can not be 0!")
+        else:
+            result = fractional_sum(nume1, deno1, nume2, deno2)
+            print(f"{nume1}/{deno1} + {nume2}/{deno2}  ====>  {result[0]}/{result[1]}")
+
+        choice = input("Do you want to continue the program (y/n) : ")
+        if(choice.lower() == "n"):
+            print("Program finished!")
+            break
+
+except ValueError as e:
+    print(f"Invalid Input : {e}")
+
+except ZeroDivisionError as e:
+    print(f"Zero division exception : {e}")
+
+except Exception as e:
+    print(f"Exception caught : {e}")
+
