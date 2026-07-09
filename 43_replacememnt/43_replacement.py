d426008439be07318e7456f17f3410c18309bca9
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-07-09 22:14:50 +0530

    feat(logic): add module based approach for 0 replacement

--- 43_replacememnt/43_replacement.py
+++ 43_replacememnt/43_replacement.py
@@ -33,3 +33,48 @@ try:
     print("".join(s))
 except:
     print("Enter a valid integer value!!!")
+
+# method 4 - using module based approach
+
+def replace_zeros(num):
+    if (num == 0):
+        return 1
+    place = 1
+    result = 0
+    while (num > 0):
+        digit = num % 10 # extracting last digit for operation
+        if (digit == 0):
+            digit = 1
+        result += (place * digit)
+        place *= 10 # calculating digit's place on each iteration
+        num //= 10 # updating num with remaing digit except last digit
+    return result
+
+try:
+    while (True):
+        num = int(input("Enter a Integer value: "))
+    
+        result = replace_zeros(abs(num))
+        if (num < 0):
+            print(f"{num} ===> {result * -1}")
+        else:
+            print(f"{num} ===> {result}")
+
+        choice = input("Do you want to continue the program (y/n) : ")
+        if(choice.lower() == "n"):
+            print("Program finished!")
+            break
+
+except ValueError as e:
+    print(f"Invalid Input : {e}")
+
+except ZeroDivisionError as e:
+    print(f"Zero division exception : {e}")
+
+except Exception as e:
+    print(f"Exception caught : {e}")
+
+
+
+
+
