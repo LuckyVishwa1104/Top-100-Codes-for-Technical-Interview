9ae572b5fcd7b88c744c01c3514662bae1963ecb
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-07-14 19:21:33 +0530

    feat(logic): add module based approach for area of circle

--- 46_area_of_circle/46_area_of_circle.py
+++ 46_area_of_circle/46_area_of_circle.py
@@ -1,4 +1,5 @@
 # Python program to find area of circle
+import math
 
 # method 1 - using formula
 try:
@@ -15,3 +16,30 @@ try:
     print("area is =",area)
 except:
     print("Enter a valid input...")
+
+# method. 3 - using module based approach
+
+def circle_area(radius):
+    return lambda radius : (math.pi * (radius ** 2))
+
+try:
+    while (True):
+        num = int(input("Enter a Integer value: "))
+
+        result = circle_area(abs(num))
+        print(f"Area of Circle with radius {num} : {result}")
+
+        choice = input("Do you want to continue the program (y/n) : ")
+        if(choice.lower() == "n"):
+            print("Program finished!")
+            break
+
+except ValueError as e:
+    print(f"Invalid Input : {e}")
+
+except ZeroDivisionError as e:
+    print(f"Zero division exception : {e}")
+
+except Exception as e:
+    print(f"Exception caught : {e}")
+
