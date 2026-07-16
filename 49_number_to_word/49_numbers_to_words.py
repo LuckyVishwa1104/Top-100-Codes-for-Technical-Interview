00029314a9c84a2a2b9ea05de7cbbedc4d942051
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-07-16 20:45:16 +0530

    feat(logic): add module based approach for digit to number conversion

--- 49_number_to_word/49_numbers_to_words.py
+++ 49_number_to_word/49_numbers_to_words.py
@@ -21,7 +21,146 @@ for i,j in zip(places,l):
 print(res)
 
 # method 2 - usin gbuilt-in library
-from num2words import num2words
+from num2words import num2words # type: ignore
 n=int(input("Enter the umber :"))
 print(num2words(n))
+
+# nethod 3 - using module based approach
+
+def convert_two_digit(num):
+
+    # dictionaries for digit places
+    ones = {
+        0: "",
+        1: "One",
+        2: "Two",
+        3: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine"
+    }
+
+    teens = {
+        10: "Ten",
+        11: "Eleven",
+        12: "Twelve",
+        13: "Thirteen",
+        14: "Fourteen",
+        15: "Fifteen",
+        16: "Sixteen",
+        17: "Seventeen",
+        18: "Eighteen",
+        19: "Nineteen"
+    }
+
+    tens ={
+        2: "Twenty",
+        3: "Thirty",
+        4: "Forty",
+        5: "Fifty",
+        6: "Sixty",
+        7: "Seventy",
+        8: "Eighty",
+        9: "Ninety"
+    }
+
+    if (num < 10):
+        return ones[num]
+    
+    elif(num < 20):
+        return teens[num]
+    
+    else:
+        ten = num // 10
+        one = num % 10
+
+        if (0 == one):
+            return tens[ten]
+        
+        return tens[ten]+ " " +ones[one]
+
+
+def convert_three_digit(num):
+    if (num < 100):
+        return convert_two_digit(num)
+    
+    hundred = num // 100
+    remainder = num % 100
+
+    result = ""
+
+    result += convert_two_digit(hundred) + " Hundred"
+
+    if (0 != remainder):
+        result += " " + convert_two_digit(remainder)
+
+    return result
+
+def convert_four_digit(num):
+
+    if (num < 1000):
+        return convert_three_digit(num)
+    
+    thausand = num // 1000
+    remainder = num % 1000
+
+    result = ""
+
+    result += convert_three_digit(thausand) + " Thausand"
+
+    if (0 != remainder):
+        result += " " + convert_three_digit(remainder)
+
+    return result
+
+def conver_six_digit(num):
+
+    if (0 == num):
+        return "Zero"
+    
+    million = num // 1000000
+    remainder = num % 1000000
+
+    result = ""
+    result += convert_four_digit(million) + " Million"
+
+    if (0 != remainder):
+        result += " " + convert_four_digit(remainder)
+
+    return result
+
+def digit_to_words(num):
+    return conver_six_digit(num)
+
+try:
+    while (True):
+        
+        num = int(input("Enter number : "))
+
+        if(num < 0):
+            print("Enter positive Integer Value")
+
+        else:
+
+            result = digit_to_words(num)
+            print(f"{num}  ===>  {result}")
+
+        choice = input("Do you want to continue the program (y/n) : ")
+        if(choice.lower() == "n"):
+            print("Program finished!")
+            break
+
+except ValueError as e:
+    print(f"Invalid Input : {e}")
+
+except ZeroDivisionError as e:
+    print(f"Zero division exception : {e}")
+
+except Exception as e:
+    print(f"Exception caught : {e}")
+
+
 
