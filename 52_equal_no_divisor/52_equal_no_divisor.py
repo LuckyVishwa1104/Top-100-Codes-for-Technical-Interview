b0b767a024114592af3f9f896b89d204849f038a
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-07-18 19:21:15 +0530

    feat(logic): add module based approach for equal divisor problem

--- 52_equal_no_divisor/52_equal_no_divisor.py
+++ 52_equal_no_divisor/52_equal_no_divisor.py
@@ -23,7 +23,7 @@ try:
     m=int(input("Enter the number of divisor :"))
     cnt=0
     def func1(a):
-        if a>n:
+        if a>n: 
             return
         cnt_ind=0
         def func2(b):
@@ -42,3 +42,50 @@ try:
     print(cnt)
 except:
     print("Something went wrong!!!")
+
+# method 3 - using module based approach
+
+def equal_divisor(num, divi):
+    if (divi == 1):
+        return 1
+    count = 0
+    for i in range(2, num + 1):
+        equal_count = True
+        count_factor = 0
+        for j in range(1, i + 1):
+            if (i % j == 0):
+                count_factor += 1
+                if (divi < count_factor):
+                    equal_count = False
+                    break
+        if (equal_count):
+            count += 1
+    return count
+
+try:
+    while (True):
+        
+        number = int(input("Enter number : "))
+        divisor = int(input("Enter divisor : "))
+
+        if(divisor <= 0 or number <= 0):
+            print("Enter positive value")
+        else:
+            result = equal_divisor(number, divisor)
+            print(f"{result} numbers have {divisor} divisors.")
+
+        choice = input("Do you want to continue the program (y/n) : ")
+        if(choice.lower() == "n"):
+            print("Program finished!")
+            break
+
+except ValueError as e:
+    print(f"Invalid Input : {e}")
+
+except ZeroDivisionError as e:
+    print(f"Zero division exception : {e}")
+
+except Exception as e:
+    print(f"Exception caught : {e}")
+
+
