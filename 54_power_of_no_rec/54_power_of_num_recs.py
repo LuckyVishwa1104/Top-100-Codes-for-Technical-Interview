dc6239a9ebc2d6eb84329f018545fb5f5c642bc4
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-07-20 20:02:11 +0530

    feat(logic): with exception handling

--- 54_power_of_no_rec/54_power_of_num_recs.py
+++ 54_power_of_no_rec/54_power_of_num_recs.py
@@ -28,3 +28,36 @@ try:
 except:
     print("Something went wrong!!!")
 
+# method 3 - using module based approach
+
+try:
+    while (True):
+        
+        number = int(input("Enter number : "))
+        pow = int(input("Enter power factor : "))
+
+        result = 1
+        def pow(num):
+            if (num > pow):
+                return
+            global result
+            result *=  number
+            pow(num + 1)
+        pow(1)
+
+        print(f"{number} ^ {pow} = {result}.")
+
+        choice = input("Do you want to continue the program (y/n) : ")
+        if(choice.lower() == "n"):
+            print("Program finished!")
+            break
+
+except ValueError as e:
+    print(f"Invalid Input : {e}")
+
+except ZeroDivisionError as e:
+    print(f"Zero division exception : {e}")
+
+except Exception as e:
+    print(f"Exception caught : {e}")
+
