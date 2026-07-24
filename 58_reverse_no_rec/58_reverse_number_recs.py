5e4d7bc11c0c17824607f980d87e1677f61fbf1e
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-07-24 15:55:05 +0530

    feat(logic): add exception based module approach

--- 58_reverse_no_rec/58_reverse_number_recs.py
+++ 58_reverse_no_rec/58_reverse_number_recs.py
@@ -17,4 +17,34 @@ try:
     print(int(s))
 except:
     print("Something went wrong!!!")
-    
+
+# method 2 - using module based approach and exception 
+
+try:
+    while(True):
+        number = int(input("Enter number : "))
+        rev_num = 0
+        def rev_number(num):
+            if (num <= 0):
+                return
+            last_digit = num % 10
+            global rev_num
+            rev_num = (rev_num * 10) + last_digit
+            rev_number(num // 10)
+        rev_number(number)
+
+        print(f"{number} ===> {rev_num}")
+
+        choice = input("Do you want to continue the program (y/n) : ")
+        if(choice.lower() == "n"):
+            print("Program finished!")
+            break
+
+except ValueError as ve:
+    print(f"Invalid input - {ve}")
+
+except ZeroDivisionError as zde:
+    print(f"Zero Division Exception - {zde}")
+
+except Exception as e:
+    print(f"Exception caught - {e}")
