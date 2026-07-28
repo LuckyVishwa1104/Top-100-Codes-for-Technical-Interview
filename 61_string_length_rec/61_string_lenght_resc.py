1666a98a163e9906b112a1ca7af0ebc1dc4a348e
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2026-07-28 19:12:36 +0530

    feat(logic): add exception and recursion based approach for string lenght

--- 61_string_length_rec/61_string_lenght_resc.py
+++ 61_string_length_rec/61_string_lenght_resc.py
@@ -12,3 +12,31 @@ def string_lenght(s,d=0):
 string_lenght(s)
 print(a)
 
+# method 2 - using standard recursion and exception
+try:
+    while(True):
+        parent_string = (input("Enter String value : "))
+
+        def str_length(shallow_string, ind):
+            if (shallow_string == parent_string):
+                return ind
+            return str_length(shallow_string+parent_string[ind], ind + 1)
+
+        str_len = str_length("", 0)
+
+        print(str_len)
+
+        choice = input("Do you want to continue the program (y/n) : ")
+        if(choice.lower() == "n"):
+            print("Program finished!")
+            break
+
+except ValueError as ve:
+    print(f"Invalid input - {ve}")
+
+except ZeroDivisionError as zde:
+    print(f"Zero Division Exception - {zde}")
+
+except Exception as e:
+    print(f"Exception caught - {e}")
+
